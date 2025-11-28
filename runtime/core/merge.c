2d82e12753b4483cc07f5d0021ff2f7ad783366f
tokyo4j <hrak1529@gmail.com>
2025-11-28 17:33:16 +0900

    Fix rb_next() misuse

--- runtime/core/merge.c
+++ runtime/core/merge.c
@@ -359,26 +359,22 @@ merge_handle_data_destroy(uint64_t ipa) {
 
 	for (struct rb_node *node = rb_first(ctx->mergeable_pages);
 			node; node = rb_next(node)) {
-		for (struct rb_node *chain = node; chain; chain = chain->dup) {
-			struct page_item *item = rb2item(chain);
+		struct page_item *item = rb2item(node);
 			for (struct page_ref *ref = item->refs; ref; ref = ref->next) {
 				if (ref->ipa == ipa) {
 					NOTICE("Tried to destroy mergeable page: ipa=%lx, pa=%lx\n",
 						ref->ipa, item->pa);
-				}
 			}
 		}
 	}
 
 	for (struct rb_node *node = rb_first(ctx->reclaimed_pages);
 			node; node = rb_next(node)) {
-		for (struct rb_node *chain = node; chain; chain = chain->dup) {
-			struct page_item *item = rb2item(chain);
+		struct page_item *item = rb2item(node);
 			for (struct page_ref *ref = item->refs; ref; ref = ref->next) {
 				if (ref->ipa == ipa) {
 					NOTICE("Tried to destroy reclaimed page: ipa=%lx, pa=%lx\n",
 						ref->ipa, item->pa);
-				}
 			}
 		}
 	}
