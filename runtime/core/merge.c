bab716dd8e06f45507f15c1aa04aca4c8fab5738
tokyo4j <hrak1529@gmail.com>
2025-12-09 10:05:54 +0900

    init random seed from timer & remove min delay

--- runtime/core/merge.c
+++ runtime/core/merge.c
@@ -67,7 +67,11 @@ rb2item(struct rb_node *node) {
 static uint32_t
 rand(void)
 {
-	static uint32_t x = 0x51810bd5;
+	static uint32_t x = 0;
+	if (x == 0) {
+		x = get_time_ns() & 0xffffffff;
+	}
+
 	x ^= x << 13;
 	x ^= x >> 17;
 	x ^= x << 5;
@@ -109,7 +113,7 @@ set_page_mergeable(struct ctx *ctx, struct rec *rec, uint64_t ipa)
 	new_item->refs = new_ref;
 	new_item->pa = pa;
 
-	uint64_t time_thr = 30 + 30 * (uint64_t)rand() / (uint64_t)UINT32_MAX;
+	uint64_t time_thr = 30 * (uint64_t)rand() / (uint64_t)UINT32_MAX;
 	time_thr *= 1000000000;
 	new_item->ns = new_item->rb.key = get_time_ns() + time_thr;
 	// NOTICE("new_item->ns=%ld\n", new_item->ns);
